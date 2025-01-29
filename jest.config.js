da12b6e99ed2b5b2372aaf9bd7d0c9dbb6aba78f
Marek Sierociński <marek.sierocinski@cdq.com>
2025-01-29 17:22:02 +0100

    build: Add test workflow

--- jest.config.js
+++ jest.config.js
@@ -10,4 +10,18 @@ module.exports = {
   modulePathIgnorePatterns: [
       "__mocks__"
   ],
+
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: './test-results/junit',
+        suiteName: 'Unit Tests',
+        classNameTemplate: '{suitename}',
+        titleTemplate: '{classname}-{title}',
+        usePathForSuiteName: 'true',
+      },
+    ],
+  ],
 };
